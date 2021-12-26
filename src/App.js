b9c66cebb4cd56be28ecfa6eabfb9f4e80edd583
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-26 18:56:43 +0300

    TourList Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './Components/Navbar/Navbar';
+import TourList from './Components/TourList/TourList';
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <TourList />
     </div>
   );
 }
---
+++ src/Components/TourList/TourList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './TourList.scss';
+
+
+export default class TourList extends Component {
+    render() {
+        return (
+            <>
+              <section>
+                Tour  
+            </section>  
+            </>
+        )
+    }
+}
