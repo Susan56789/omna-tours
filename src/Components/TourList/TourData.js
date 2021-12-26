1924f72fe7b1fd08b060225250ae50a24a289ae0
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-26 21:56:32 +0300

    tour data

--- src/Components/TourList/TourData.js
+++ src/Components/TourList/TourData.js
@@ -4,21 +4,21 @@ export const TourData =[
         city: "Nairobi",
         img:"./images/Nairobi.jpg",
         name:"nairoby city tour",
-        info:"Come tour the beautiful sights of Nairobi."
+        info:"Known as the safari capital of Africa, Nairobi is an energetic, modern city that serves as a fascinating introduction to both wildlife and nightlife. Music clubs pulse with life, shops and markets bustle, and a mélange of restaurants will tempt your palate in this former Maasai watering hole. Top sights include the Karen Blixen Museum, Giraffe Centre and the black rhinos of Nairobi National Park. The compact city center is safe to walk in and taxis make other areas accessible."
     },
     {
         id:2,
         city: "Mombasa",
         img:"./images/Mombasa.jpg",
         name:"mombasa city tour",
-        info:"Come tour the beautiful scenes of Mombasa."
+        info:"Mombasa, with a population of 900,000, is no sleepy seaside village. Its beachfront hotels appeal to travelers in search of sun, sand and surf, while its Arab, Indian and colonial European heritage makes for a wide variety of sights to see. Can't find a taxi? Travel by tuk-tuk, a three-wheeled auto rickshaw."
     },
     {
         id:3,
         city: "Kisumu",
         img:"./images/Kisumu.jpg",
         name:"kisumu city tour",
-        info:"Come enjoy the beautiful lake-view of kisumu."
+        info:"Kisumu is Kenya's third largest city and is located in the Western Highlands on Lake Victoria. Kisumu's status as a stronghold of the opposition has negatively impacted its development, as the area was largely neglected until the emergence of multi party democracy in Kenya."
     }
 
 ]
