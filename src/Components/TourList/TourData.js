4fffcbcbf0b1f1c6670d1cb5b30401c1c189a8e4
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-26 23:43:15 +0300

    tour data

--- src/Components/TourList/TourData.js
+++ src/Components/TourList/TourData.js
@@ -3,22 +3,179 @@ export const TourData =[
         id:1,
         city: "Nairobi",
         img:"./images/Nairobi.jpg",
-        name:"nairoby city tour",
+        name:"nairoby city",
         info:"Known as the safari capital of Africa, Nairobi is an energetic, modern city that serves as a fascinating introduction to both wildlife and nightlife. Music clubs pulse with life, shops and markets bustle, and a mélange of restaurants will tempt your palate in this former Maasai watering hole. Top sights include the Karen Blixen Museum, Giraffe Centre and the black rhinos of Nairobi National Park. The compact city center is safe to walk in and taxis make other areas accessible."
     },
     {
         id:2,
         city: "Mombasa",
         img:"./images/Mombasa.jpg",
-        name:"mombasa city tour",
+        name:"mombasa city ",
         info:"Mombasa, with a population of 900,000, is no sleepy seaside village. Its beachfront hotels appeal to travelers in search of sun, sand and surf, while its Arab, Indian and colonial European heritage makes for a wide variety of sights to see. Can't find a taxi? Travel by tuk-tuk, a three-wheeled auto rickshaw."
     },
     {
         id:3,
         city: "Kisumu",
         img:"./images/Kisumu.jpg",
-        name:"kisumu city tour",
+        name:"kisumu city ",
         info:"Kisumu is Kenya's third largest city and is located in the Western Highlands on Lake Victoria. Kisumu's status as a stronghold of the opposition has negatively impacted its development, as the area was largely neglected until the emergence of multi party democracy in Kenya."
-    }
+    },
+    {
+        id:4,
+        city:"Maasai Mara",
+        img: "./images/Maasai-mara.jpg",
+        name:"Maasai Mara National Reserve",
+        info:`Maasai Mara National Reserve (also "Masai Mara") is one of Africa's most magnificent game reserves. Bordering Tanzania, the Mara is the northern extension of the Serengeti and forms a wildlife corridor between the two countries.
 
+        It's named after the statuesque, red-cloaked Maasai people who live in the park and graze their animals here, as they have done for centuries. In their language, Mara means "mottled," perhaps a reference to the play of light and shadow from the acacia trees and cloud-studded skies on the vast grasslands.
+        
+        The park is famous for the Great Migration, when thousands of wildebeest, zebra, and Thomson's gazelle travel to and from the Serengeti, from July through October.`
+
+    },
+    {
+        id:5,
+        city:"Amboseli",
+        img:"./images/amboseli.jpg",
+        name:"Amboseli National Reserve",
+        info:`Crowned by Mount Kilimanjaro, Africa's highest peak, Amboseli National Reserve is one of Kenya's most popular tourist parks. The name "Amboseli" comes from a Maasai word meaning "salty dust," an apt description for the park's parched conditions.
+
+        The reserve is one of the best places in Africa to view large herds of elephants up close. Other wildlife commonly spotted in the park includes big cats, such as lion and cheetah, as well as giraffe, impala, eland, waterbuck, gazelle, and more than 600 species of birds.`
+    },
+    {
+        id:6,
+        city:"Tsavo",
+        img:"./images/tsavo-national-park.jpg",
+        name:"Tsavo National Park",
+        info:`Kenya's largest park, Tsavo, is sliced in two: Tsavo West and Tsavo East. Together these parks comprise four percent of the country's total area and encompass rivers, waterfalls, savannah, volcanic hills, a massive lava-rock plateau, and an impressive diversity of wildlife.
+
+        Midway between Nairobi and Mombasa, Tsavo East is famous for photo-worthy sightings of large elephant herds rolling and bathing in red dust. The palm-fringed Galana River twists through the park, providing excellent game viewing and a lush counterpoint to the arid plains.`
+    },
+    {
+        id:7,
+        city:"Samburu",
+        img:"./images/samburu-national-park.jpg",
+        name:"Samburu, Buffalo Springs, and Shaba National Reserves",
+        info:`On the banks of the palm-lined Ewaso Nyiro River, Samburu, Buffalo Springs, and Shaba Reserves lie in an arid region in the remote north of Kenya.
+
+        Shaba National Reserve is one of two areas where George and Joy Adamson raised Elsa the lioness, made famous in the film Born Free.
+        
+        The wildlife in all three reserves depends on the waters of the river to survive, and many species are specially adapted to the parched conditions. These include Grevy's zebras; Somali ostriches; and gerenuks, the long-necked antelope that stand on two rear legs to reach the fresh shoots on upper tree limbs.
+        
+        A top attraction in Samburu National Reserve are the Sarara Singing Wells, local watering holes where Samburu warriors sing traditional songs while hauling water for their cattle to drink. You might also be rewarded with sightings of big cats and wild dogs.`
+    },
+    {
+        id:8,
+        city:"Nakuru",
+        img:"./images/lake-nakuru-national-park.jpg",
+        name:"Lake Nakuru National Park",
+        info:`Lake Nakuru National Park, in Central Kenya, is famous for its huge flocks of pink flamingos. The birds throng on Lake Nakuru itself, one of the Rift Valley soda lakes that covers almost a third of the park's area.
+
+        The park was established in 1961, and more than 450 species of birds have been recorded here, as well as a rich diversity of other wildlife. Lions, leopards, warthogs, waterbucks, pythons, and white rhinos are just some of the animals you might see, and the landscapes range from sweeping grasslands bordering the lake to rocky cliffs and woodland.
+        
+        The park also protects the largest euphorbia candelabrum forest in Africa. These tall, branching succulents are endemic to the region and provide a bold textural element to the arid landscapes.`
+    },
+    {
+        id:9,
+        city:"Lamu",
+        img:"./images/kenya-lamu.jpg",
+        name:" Lamu Island",
+        info:`The small island of Lamu, northeast of Mombasa, oozes old-world charm. A UNESCO World Heritage Site, Lamu Old Town is Kenya's oldest continually inhabited settlement, with origins dating back to the 12th century.
+
+        Strolling the labyrinthine streets is one of the top things to do here. You can see the island's rich trading history reflected in the buildings. Architectural features from the Arab world, Europe, and India are evident, yet with a discernible Swahili technique. Intricately carved wooden doors, coral stone buildings, hidden courtyards, verandas, and rooftop patios are common features.
+        
+        Sightseeing here is like stepping back in time. Dhows plow the harbor, few if any motorized vehicles exist here, and donkeys still rule the streets as they have done for centuries. Most of Lamu's population is Muslim, and both men and women dress in traditional attire.`
+    },
+    {
+        id:10,
+        city:"Naivasha",
+        img:"./images/lake-naivasha.jpg",
+        name:"Lake Naivasha",
+        info:`A haven for birders, Lake Naivasha lies at the highest point of the Great Rift Valley. More than 400 species of birds have been spotted here, including African fish eagles, jacanas, white-fronted bee-eaters, and several species of kingfishers.
+
+        One of the best ways to view the wildlife is by boat. Hippos slosh in the water, and giraffes, zebras, buffaloes, and eland graze around the edges of the lake. Keep a lookout for colobus monkeys in the canopies, too.
+        
+        Near Lake Naivasha, the Crater Lake Game Sanctuary features a wildlife-rich nature trail.
+        
+        Just south of Lake Naivasha, the relatively affordable Hell's Gate National Park protects a wide variety of wildlife and offers excellent climbing opportunities, with two extinct volcanoes and the red cliffs of Hell's Gate Gorge.
+        
+        On the southern shore of Lake Naivasha, you can pop in for a cup of tea at the Elsamere Conservation Centre, the former home of the late Joy Adamson, author of Born Free, and her husband George.`
+    },
+    {
+        id:11,
+        city:"Nairobi",
+        img:"./images/nairobi-national-park.jpg",
+        name:"Nairobi National Park",
+        info:`Who says you need to go far from Nairobi to enjoy a safari? A mere 15-minute drive from the clamor of Kenya's capital, you can gaze at a snoozing pride of lions or a graceful giraffe strutting through the golden grass at Nairobi National Park.
+
+        Visiting this wildlife-rich park is one of the top things to do if you're staying in Nairobi, and it makes a rewarding day trip - especially if you can't make it to one of the larger game reserves.
+        
+        All the classic safari stars here, including buffalo, leopard, zebras, wildebeest, hippos, elephants, and cheetah, and you can also see some of the planet's most endangered species at the park's rhino sanctuary.
+        
+        The Nairobi Safari Walk provides a rewarding opportunity to spot wildlife on foot, and birders will be happy to know that more than 400 species of birds also inhabit the park, including the beautiful grey crowned crane. And no visit to the park would be complete without popping into the David Sheldrick Wildlife Trust Elephant Nursery at the park's main gates. Also save time to pop into Giraffe Centre, near the famous Giraffe Manor, where these long-necked beauties eat right out of your hands.`
+    },
+{
+    id:12,
+    city:"Nairobi",
+    img:"./images/david-sheldrick-wildlife-trust-elephant-nursery.jpg",
+    name:"David Sheldrick Wildlife Trust Elephant Nursery",
+    info:`Who can resist a baby elephant? At the David Sheldrick Wildlife Trust, you can mingle with impossibly cute baby pachyderms and feel good about supporting an important conservation organization at the same time.
+
+    This famous wildlife sanctuary rescues and rehabilitates orphaned elephants and offers you the chance to see these lovable creatures up close. This is where the youngest elephants are hand-reared until about two or three years of age, and you can watch the keepers bottle-feed the babies.
+    
+    From the sanctuary, staff relocate the elephants to a reintegration center in Tsavo East National Park before they are eventually released back into the wild.
+    
+    Watching these playful pint-sized pachyderms wallow in the mud or nudge a soccer ball around is something you will never forget. If you want to do more to help these magnificent animals, consider adopting an orphaned elephant, giraffe, or rhino before you go, and you'll receive regular updates on its progress.
+    
+    Conveniently, the nursery sits right at the main gates of Nairobi National Park, so you can visit both of these famous tourist attractions in the same day.`
+},
+{
+    id:13,
+    city:"Malindi",
+    img:"./images/malindi.jpg",
+    name:"Malindi",
+    info:`North of Mombasa on the Kenyan coast, Malindi has a split personality. This popular beach town is part historic old town, part modern tourist hub. Thanks to its rich trading history, it is also a melting pot of cultures and cuisines.
+
+    Travelers, many from Europe, come here to sun on the white sands of Watamu Beach and dive the coral reefs of the Malindi and Watamu Marine National Parks. If you're looking for free things to do in Kenya, you can do worse than bask on a beautiful Malindi Beach.
+    
+    You can also soak up a dose of Swahili history in the historic town, which dates from the 12th century. Here, you can visit the Jami Mosque; two pillar tombs from the 14th century; and the Church of St. Francis Xavier, one of East Africa's oldest churches.`
+},
+{
+    id:14,
+    city:"Central Kenya",
+    img:"./images/mount-kenya-national-park.jpg",
+    name:"Mount Kenya National Park",
+    info:`In the Central Highlands, east of the Great Rift Valley, Mount Kenya National Park is a UNESCO World Heritage Site that provides the rare spectacle of equatorial snow. It encompasses the country's namesake highest mountain at 5,199 meters.
+
+    Formed by a series of volcanic eruptions, Mount Kenya is actually comprised of three glacier-cloaked peaks. The highest is Batian, although Nelion, the next highest, is a tougher climb. The lowest peak, Lenana, is considered the easiest climb, although unpredictable weather can pose challenges.
+    
+    Bring your camera. The striking scenery varies from glaciers, lakes, and mineral springs to alpine forest and dense pockets of bamboo.
+    
+    The diversity of flora and fauna provides rewarding opportunities for safaris. Among the wildlife here, you may spot black and white colobus monkeys, buffalo, elephant, tree hyrax, leopard, and hyena.`
+},
+{
+    id:15,
+    city:"Naivasha",
+    img:"./images/hells-gate-national-park.jpg",
+    name:"Hell's Gate National Park",
+    info:`A hot spot for climbers, Hell's Gate National Park is one of the few parks in Kenya that allows camping and enables you to explore on foot or bicycle.
+
+    Hell's Gate offers excellent climbing and hiking opportunities, with two extinct volcanoes; the red cliffs of Hell's Gate Gorge; ObsidianCaves; and the pointed column of rock known as Fischer's Tower, a former volcanic plug.
+    
+    Geothermal features include hot springs and natural geysers hissing steam through vents in the earth's crust. The park also protects a wide variety of wildlife, including leopards, baboons, hartebeest, eland, ostriches, gazelles, and more than 100 species of birds. Eagle and vulture breeding grounds also lie within the park.
+    
+    The Oloor Karia Maasai Cultural Centre within the park is also worth a visit, with Maasai singing, dancing, and jewelry-making demonstrations.`
+},
+{
+    id:16,
+    city:"Nanyuki",
+    img:"./images/ol-pejeta-conservancy.jpg",
+    name:"Ol Pejeta Conservancy",
+    info:`About 200 kilometers north of Nairobi, near Mount Kenya National Park, Ol Pejeta Conservancy is a prime place for close-up wildlife encounters.
+
+    Conservation and sustainability are key at this 90,000-acre private game reserve, where you can view the Big Five (lion, leopard, rhino, elephant, and buffalo), as well as other animals such as cheetah, hyenas, zebra, and hartebeest - all set against the breathtaking backdrop of snowcapped Mount Kenya.
+    
+    The conservancy is perhaps best known for its northern and southern white rhinos, including Baraka, a blind black rhino, who lucky visitors might have the chance to feed.
+    
+    You can view the wildlife on self-drive or guided tours, and entry includes a visit to the chimpanzee sanctuary. `
+}
 ]
